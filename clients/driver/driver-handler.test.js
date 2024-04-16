ea7759c7a72cd407170b8f530a7f287becc9f46a
Isai Chaidez <isaichaidez@gmail.com>
2024-04-16 15:02:36 -0700

    updated tests to include timeouts" "
    "

--- clients/driver/driver-handler.test.js
+++ clients/driver/driver-handler.test.js
@@ -1,29 +1,37 @@
-const driverHandler = require("./handler.js");
+const { transit, delivered } = require("./handler.js");
 
 describe("Driver handler", () => {
   it("should return transit emit and console.log", () => {
-    const emitter = {
+    const socket = {
       emit: jest.fn(),
     };
     const payload = { orderID: 123 };
     console.log = jest.fn();
 
-    driverHandler.transit(emitter, payload);
+    transit(socket, payload);
 
-    expect(emitter.emit).toHaveBeenCalledWith("inTransit", payload);
-    expect(console.log).toHaveBeenCalledWith("DRIVER", "picked up", 123);
+    setTimeout(() => {
+      expect(console.log).toHaveBeenCalledWith("DRIVER", "picked up", payload);
+    }, 2000);
+    setTimeout(() => {
+      expect(socket.emit).toHaveBeenCalledWith("inTransit", payload);
+    }, 3000);
   });
 
   it("should return delivered emit and console.log", () => {
-    const emitter = {
+    const socket = {
       emit: jest.fn(),
     };
     const payload = { orderID: 321 };
     console.log = jest.fn();
 
-    driverHandler.delivered(emitter, payload);
+    delivered(socket, payload);
 
-    expect(emitter.emit).toHaveBeenCalledWith("delivered", payload);
-    expect(console.log).toHaveBeenCalledWith("DRIVER", "delivered", 321);
+    setTimeout(() => {
+      expect(console.log).toHaveBeenCalledWith("DRIVER", "delivered", payload);
+    }, 2000);
+    setTimeout(() => {
+      expect(socket.emit).toHaveBeenCalledWith("delivered", payload);
+    }, 3000);
   });
 });
